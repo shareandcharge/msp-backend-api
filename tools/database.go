f7bec209d86bda0b0810e19a278062f9e5b91a2d
Andy <danandreist@gmail.com>
2018-08-17 14:09:00 +0300

    hope not break

--- tools/database.go
+++ tools/database.go
@@ -6,18 +6,19 @@ import (
 	"time"
 )
 
-type CPO struct {
-	CpoId     int    `db:"cpo_id" json:"cpo_id"`
-	Wallet    string `db:"wallet" json:"wallet"`
-	Seed      string `db:"seed" json:"seed"`
-	Name      string `db:"name" json:"name"`
-	Address1  string `db:"address_1" json:"address_1"`
-	Address2  string `db:"address_2" json:"address_2"`
-	Town      string `db:"town" json:"town"`
-	Postcode  string `db:"postcode" json:"postcode"`
-	MailAddr  string `db:"mail_address" json:"mail_addr"`
-	Website   string `db:"website" json:"website"`
-	VatNumber string `db:"vat_number" json:"vat_number"`
+type MSP struct {
+	MspId       int    `db:"msp_id" json:"msp_id"`
+	Wallet  string `db:"wallet" json:"wallet"`
+	Seed string `db:"seed" json:"seed"`
+	Name       string `db:"name" json:"name"`
+	Address1    string `db:"address_1" json:"address_1"`
+	Address2    string `db:"address_2" json:"address_2"`
+	Town    string `db:"town" json:"town"`
+	Postcode    string `db:"postcode" json:"postcode"`
+	MailAddr    string `db:"mail_address" json:"mail_addr"`
+	Website    string `db:"website" json:"website"`
+	VatNumber    string `db:"vat_number" json:"vat_number"`
+
 }
 
 var DB *sqlx.DB
