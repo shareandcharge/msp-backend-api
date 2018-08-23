768c5ad13f77aecc0d419264b3d1db9919bc1e4b
Andy <danandreist@gmail.com>
2018-08-23 16:47:58 +0200

    fixin bugs all day long........

--- handlers/msp.go
+++ handlers/msp.go
@@ -147,7 +147,7 @@ func MSPHistory(c *gin.Context) {
 			tools.ErrorCheck(err, "cpo.go", false)
 			calculatedGas := tools.HexToUInt(txResponse.GasUsed) * tools.HexToUInt(tx.GasPrice)
 
-			histories = append(histories, History{Block: tx.BlockNumber, FromAddr: tx.From, ToAddr: tx.To, Amount: calculatedGas * 10000000000, Currency: "wei", CreatedAt: tx.Timestamp, TransactionHash: tx.Hash})
+			histories = append(histories, History{Block: tx.BlockNumber, FromAddr: tx.From, ToAddr: tx.To, Amount: calculatedGas * 100000000000, Currency: "wei", CreatedAt: tx.Timestamp, TransactionHash: tx.Hash})
 
 		} else {
 			//we have eth transfer
